31229d7077455c604735813cbdec65b744b517c4
Sanduni Deenalattha <78231679+Sanduni522@users.noreply.github.com>
2022-09-14 12:36:19 -0500

    Update SASA.py

--- SASA.py
+++ SASA.py
@@ -10,7 +10,7 @@ resname = ATOM['residue_name']
 atom = ATOM['atom_name']
 resi_number = ATOM['residue_number']
 length = len(ATOM.index)
-print("Nt   SASA value")
+print("Nt,Nt_num,SASA value")
 for i in range(length):
     if atom[i] == 'N1':
         if (resname[i] == 'A'):
@@ -19,7 +19,7 @@ for i in range(length):
             selection = freesasa.selectArea((f'n1,(name N1) and (resn A) and (resi {resi_number[i]}) ',f'n3, (name N3) and (resn C) and (resi {resi_number[i]})'), structure, result)
             sel1 = selection['n1']
 
-            print(resname[i],'  ',sel1)
+            print(resname[i],resi_number[i],sel1)
 
         elif(resname[i] == 'C'):
             structure = freesasa.Structure(f'{name}')
@@ -30,6 +30,6 @@ for i in range(length):
                                             result)
             sel2 = selection['n3']
 
-            print(resname[i],'  ',sel2)
+            print(resname[i],resi_number[i],sel2)
         else:
-            print(resname[i],'  ',0)
+            print(resname[i],resi_number[i],0)
